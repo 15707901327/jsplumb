1658a56c579aa44372dc1b3ed9fe9c6053e7fcec
dengym <dengym@vci-tech.com>
2019-04-11 11:42:17 +0800

    1.实例

---
+++ demo/flowchart/js/jsplumb_self.js
@@ -0,0 +1,6 @@
+$(function () {
+    /******************* UI控制部分 ***************/
+    //初始化动画效果
+    $('.chart-ratio-form,.chart-fill-border-selector,#chart-v-shadow-display,#chart-h-shadow-display,#chart-blur-range-display,#chart-shadow-blur-display,#chart-shadow-color-display,#chart-shadow-spread-display').css("opacity",'0.6');
+    console.log(123);
+});
